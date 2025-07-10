d6bcbc23af54c7d7b5c2222672ea9ba7929f0cdd
the-maus <msampaio.mail@gmail.com>
2025-07-09 22:44:34 -0300

    Testing Spring Boot logs

--- springboot-java-rest/src/main/java/br/com/maus/services/PersonService.java
+++ springboot-java-rest/src/main/java/br/com/maus/services/PersonService.java
@@ -3,19 +3,19 @@ package br.com.maus.services;
 import br.com.maus.exception.ResourceNotFoundException;
 import br.com.maus.model.Person;
 import br.com.maus.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
 
 @Service
 public class PersonService {
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonService.class.getName());
+    private Logger logger = LoggerFactory.getLogger(PersonService.class.getName());
 
     @Autowired
     PersonRepository repository;
