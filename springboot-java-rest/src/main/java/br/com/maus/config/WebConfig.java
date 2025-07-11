72bec3f6d3e53cd09f324f8afb16cd5450096ae6
the-maus <msampaio.mail@gmail.com>
2025-07-11 20:06:50 -0300

    Content Negotiation support

---
+++ springboot-java-rest/src/main/java/br/com/maus/config/WebConfig.java
@@ -0,0 +1,34 @@
+package br.com.maus.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration //this class contain configurations and may contain beans declaration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        // Via EXTENSION (http://localhost:8080/api/person/v1/3.xml or http://localhost:8080/api/person/v1/3.json) Deprecated on Spring Boot 2.6
+
+        // Via QUERY PARAM http://localhost:8080/api/person/v1/3?format=xml
+//        configurer.favorParameter(true)
+//                .parameterName("format")
+//                .ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(true)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                    .mediaType("json", MediaType.APPLICATION_JSON)
+//                    .mediaType("xml", MediaType.APPLICATION_XML);
+
+        // Via Accept Header http://localhost:8080/api/person/v1/3
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(true)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("yaml", MediaType.APPLICATION_YAML);
+    }
+}
--- springboot-java-rest/src/main/java/br/com/maus/controllers/PersonController.java
+++ springboot-java-rest/src/main/java/br/com/maus/controllers/PersonController.java
@@ -18,29 +18,48 @@ public class PersonController {
 
     @GetMapping(
         value = "/{id}",
-        produces = MediaType.APPLICATION_JSON_VALUE
+        produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE}
     )
     public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
     // when there's no value then method is returned by default when accessing /person
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {
+        MediaType.APPLICATION_JSON_VALUE,
+        MediaType.APPLICATION_XML_VALUE,
+        MediaType.APPLICATION_YAML_VALUE}
+    )
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
     @PostMapping(
-        consumes = MediaType.APPLICATION_JSON_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE
+        consumes = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE},
+        produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE}
     )
     public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
     @PutMapping(
-        consumes = MediaType.APPLICATION_JSON_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE
+        consumes = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE},
+        produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE}
     )
     public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
