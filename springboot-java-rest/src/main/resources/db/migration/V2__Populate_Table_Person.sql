25e3a75c234038babc850eab8c5f72d86b9dbb20
the-maus <msampaio.mail@gmail.com>
2025-07-11 01:04:45 -0300

    Working with Flyway migrations

---
+++ springboot-java-rest/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,13 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Dominaria', 'Liliana', 'Female', 'Vess'),
+	(3, 'Anchiano - Itália', 'Leonardo', 'Male', 'Da 20'),
+	(4, 'Porbandar - India', 'Indira', 'Female', 'Gandhi'),
+	(5, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(7, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
+	(8, 'Kentucky - US', 'Muhamad', 'Male', 'Ali'),
+	(10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(11, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(12, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(13, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(14, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla'),
+	(15, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
