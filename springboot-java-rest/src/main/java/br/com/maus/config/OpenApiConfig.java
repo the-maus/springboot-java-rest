24cfeb597020ee9e5cbd98e9dd81c089144c9d01
the-maus <msampaio.mail@gmail.com>
2025-11-20 15:56:32 -0300

    Continuous Deployment with Google Kubernetes Engine (GKE) and GitHub Actions

--- springboot-java-rest/src/main/java/br/com/maus/config/OpenApiConfig.java
+++ springboot-java-rest/src/main/java/br/com/maus/config/OpenApiConfig.java
@@ -1,19 +1,24 @@
 package br.com.maus.config;
 
+import br.com.maus.services.InstanceInformationService;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Autowired
+    InstanceInformationService service;
+
     @Bean
     OpenAPI customOpenAPI() {
         return new OpenAPI()
             .info(new Info()
-                .title("Java Spring Boot REST API")
+                .title("Java Spring Boot REST API - v1 " + service.retrieveInstanceInfo())
                 .version("v1")
                 .description("RESTful API from 0 with Java, Spring Boot, Kubernetes and Docker")
                 .termsOfService("https://spring.io/projects/spring-boot")
